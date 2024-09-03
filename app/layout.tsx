28278bbbe902bff0bfe490ade129e6d4fba54a3a
infinity-young <110441291+infinity-young@users.noreply.github.com>
2024-09-03 22:25:43 +0800

    Next学习前5章存档

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // <body className={`${inter.className} antialiased`}>{children}</body>
+console.log("======",inter)
   return (
     <html lang="en">
-      <body>{children}</body>
+    <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
